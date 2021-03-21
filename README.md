f9c8a2dfc9adbf0ca087248c4c104033c18925a4
BorONE-stud <borodin.ss@phystech.edu>
2021-03-21 03:51:07 +0300

    executable

---
+++ README.md
@@ -0,0 +1,6 @@
+
+to install
+mkdir build && cd build && cmake .. && make
+
+to uninstall
+cd .. && rm -rf build bin
---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Nothing to see here. Come later or check source." << std::endl;
+}
